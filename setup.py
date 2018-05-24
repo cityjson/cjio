6abcd0b2025ce86e70d08cb0c336217a56c518b2
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-05-24 16:24:52 +0200

    long_description in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='cjio',
     version='0.2.0',
     description='Python CLI to process and manipulate CityJSON files',
-    url='https://github.com/tudelft3d/cityjson_tools',
+    long_description='Python CLI to process and manipulate [CityJSON](http://www.cityjson.org) files. The different operators can be chained to perform several processing in one step, the CityJSON model goes through them and allows to save to a new CityJSON at the end.',
+    long_description_content_type="text/markdown",
+    url='https://github.com/tudelft3d/cjio',
     author='Hugo Ledoux, Balázs Dukai',
     author_email='h.ledoux@tudelft.nl, b.dukai@tudelft.nl',
     python_requires='>=3',
@@ -19,7 +21,7 @@ setup(
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Operating System :: POSIX :: Linux'
+        'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows'
     ],
