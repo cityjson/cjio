d325c9bcbb576a4d6957a0ea592cdb72e2ad4b2c
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-09-01 16:28:24 +0200

    Newell method with collapsed triangle caused problem
    solved by not called Newell for triangle

--- cjio/geom_help.py
+++ cjio/geom_help.py
@@ -7,7 +7,7 @@ def to_2d(p, n):
     # p = np.array([1, 2, 3])
     # newell = np.array([1, 3, 4.2])
     # n = newell/sqrt(p[0]*p[0] + p[1]*p[1] + p[2]*p[2])
-    x3 = np.array([1,1,1])    #-- is this always a good value??
+    x3 = np.array([1.1, 1.1, 1.1])    #-- is this always a good value??
     if((n==x3).all()):
         x3 += np.array([1,2,3])
     x3 = x3 - np.dot(x3, n) * n
@@ -21,6 +21,8 @@ def get_normal_newell(poly):
     # find normal with Newell's method
     # print (poly)
     n = np.array([0.0, 0.0, 0.0])
+    # if len(poly) == 0:
+    #     print ("NOPOINTS")
     for i,p in enumerate(poly):
         ne = i + 1
         if (ne == len(poly)):
@@ -28,5 +30,9 @@ def get_normal_newell(poly):
         n[0] += ( (poly[i][1] - poly[ne][1]) * (poly[i][2] + poly[ne][2]) )
         n[1] += ( (poly[i][2] - poly[ne][2]) * (poly[i][0] + poly[ne][0]) )
         n[2] += ( (poly[i][0] - poly[ne][0]) * (poly[i][1] + poly[ne][1]) )
-    n = n/sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2])    
+    
+    if (n==np.array([0.0, 0.0, 0.0])).all():
+        print ("oh collapsed triangle:", poly)
+       
+    n = n / sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2])    
     return n
