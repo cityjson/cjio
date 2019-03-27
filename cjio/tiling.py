ddc11110f3c511c79555f23fb08274d1e6fae25d
Balázs Dukai <balazs.dukai@gmail.com>
2019-03-27 14:07:43 +0100

    Implement _subdivide with iteration input

--- cjio/tiling.py
+++ cjio/tiling.py
@@ -1,11 +1,59 @@
 """Partitioning a CityJSON file"""
 
-def create_grid(cellsize: tuple(int, int)):
+import warnings
+from typing import List
+
+from cjio.cityjson import CityJSON
+
+def _subdivide_helper(bbox: List[float], iteration: int, cntr: int) -> List[List]:
+    if cntr == iteration:
+        return bbox
+    else:
+        cntr += 1
+        center_x = (bbox[0] + bbox[3]) / 2
+        center_y = (bbox[1] + bbox[4]) / 2
+        center_z = (bbox[2] + bbox[5]) / 2
+        sw_0 = [bbox[0], bbox[1], bbox[2], center_x, center_y, center_z]
+        se_0 = [center_x, bbox[1], bbox[2], bbox[3], center_y, center_z]
+        ne_0 = [center_x, center_y, bbox[2], bbox[3], bbox[4], center_z]
+        nw_0 = [bbox[0], center_y, bbox[2], center_x, bbox[4], center_z]
+        sw_1 = [bbox[0], bbox[1], center_z, center_x, center_y, bbox[5]]
+        se_1 = [center_x, bbox[1], center_z, bbox[3], center_y, bbox[5]]
+        ne_1 = [center_x, center_y, center_z, bbox[3], bbox[4], bbox[5]]
+        nw_1 = [bbox[0], center_y, center_z, center_x, bbox[4], bbox[5]]
+        return [_subdivide_helper(nw_0, iteration, cntr), _subdivide_helper(ne_0, iteration, cntr),
+                _subdivide_helper(sw_0, iteration, cntr), _subdivide_helper(se_0, iteration, cntr),
+                _subdivide_helper(nw_1, iteration, cntr), _subdivide_helper(ne_1, iteration, cntr),
+                _subdivide_helper(sw_1, iteration, cntr), _subdivide_helper(se_1, iteration, cntr)]
+
+def _subdivide(bbox: List[float], iteration: int) -> List[List]:
+    """Recursively subdivide the BBOX"""
+    return _subdivide_helper(bbox, iteration, 0)
+
+def create_grid(j: CityJSON, cellsize: List[float]) -> None:
     """Create an equal area, rectangular grid of the given cell size for the area
 
-    :param cellsize: Size of the grid cell. Values are integers and in
-     the units of the CRS of the city model. Values are provided as (x, y)
+    :param j: The city model
+    :param cellsize: Size of the grid cell. Values are floats and in
+     the units of the CRS of the city model. Values are provided as (x, y, z).
+     If you don't want to partition the city model with 3D cells, then omit the
+     z-value.
     """
+    bbox = j.update_bbox()
+    dx = bbox[3] - bbox[0]
+    dy = bbox[4] - bbox[1]
+    dz = bbox[5] - bbox[2]
+
+    if len(cellsize) > 2:
+        raise ValueError("Must provide at least 2 values for the cellsize")
+    elif len(cellsize) == 2:
+        print("2D partitioning")
+        cellsize.append(dz)
+    else:
+        print("3D partitioning")
+    if dx < cellsize[0] and dy < cellsize[1] and dz < cellsize[2]:
+        raise ValueError("Cellsize is larger than bounding box, returning")
+
 
 def partitioner():
     """Create a CityJSON for each cell in the partition"""
--- tests/test_tiling.py
+++ tests/test_tiling.py
@@ -2,11 +2,27 @@ import pytest
 
 from cjio import tiling
 
-def test_grid():
+@pytest.mark.parametrize("bbox, iteration", [
+    ([0.0, 0.0, 0.0, 1.0, 1.0, 1.0], 2),
+    ([0.0, 0.0, 0.0, 1.0, 1.0, 0.0], 2),
+])
+def test_subdivide(bbox, iteration):
+    octree = tiling._subdivide(bbox, iteration)
+    # some random tests
+    # ne_1[ne_1] top corner
+    assert octree[5][5][3:] == bbox[3:]
+    # ne_1[ne_0] z-value
+    assert octree[5][1][5] == bbox[5] - (bbox[5] / 2**iteration)
+
+
+
+def test_grid(rectangle):
     """Test that each cell has the required size"""
-    cellsize = (50, 50)
-    grid = tiling.create_grid(cellsize)
+    cellsize = (0.25, 0.25)
+    grid = tiling.create_grid(rectangle, cellsize)
     print(grid)
+    # test with sth WGS84
+    # test with RDNew
     pytest.fail("Not implemented")
 
 
