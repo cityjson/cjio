b4178f6e514efbb6a72c9b7a1c42adb1db446d58
Balázs Dukai <balazs.dukai@gmail.com>
2019-04-05 14:29:52 +0200

    Add function to verify output filenames

---
+++ cjio/utils.py
@@ -0,0 +1,28 @@
+"""Various utility functions"""
+
+import os.path
+from click import ClickException
+
+def verify_filename(filename):
+    """Verify if the provided output filename is a file or a directory"""
+    res = {'dir': False, 'path': ''}
+    if os.path.isdir(filename):
+        res['dir'] = True
+        absp = os.path.abspath(filename)
+        if os.path.exists(absp):
+            res['path'] = absp
+        else:
+            raise ClickException("Couldn't expand %s to absolute path" % filename)
+    else:
+        base = os.path.basename(filename)
+        dirname = os.path.abspath(os.path.dirname(filename))
+        # parent directory must exist, we don't recurse further
+        if not os.path.exists(dirname):
+            raise ClickException('Path does not exist: "%s"' % (dirname))
+        fname, extension = os.path.splitext(base)
+        res['path'] = os.path.join(dirname, base)
+        if len(extension) == 0:
+            res['dir'] = True
+        else:
+            res['dir'] = False
+    return res
---
+++ tests/test_utils.py
@@ -0,0 +1,34 @@
+import os.path
+import pytest
+from click import ClickException
+
+from cjio import utils
+
+
+@pytest.fixture(scope='session',
+                params=[("doesnt_exist.json", False),
+                        ("example_data/rotterdam", True),
+                        ("example_data/rotterdam/", True),
+                        ("example_data/delft.json", False),
+                        ("example_data/doesnt_exist", True)
+])
+def valid_path(request):
+    package_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    yield (os.path.join(package_dir, request.param[0]), request.param[1])
+
+@pytest.fixture(scope='session',
+                params=[("example_data/doesnt_exist/doesnt_exist.json", False),
+                        ("example_data/doesnt_exist/other_dir", True)
+])
+def invalid_path(request):
+    package_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    yield (os.path.join(package_dir, request.param[0]), request.param[1])
+
+
+def test_verify_filenames_valid(valid_path):
+    res = utils.verify_filename(valid_path[0])
+    assert res['dir'] == valid_path[1]
+
+def test_verify_filenames_invalid(invalid_path):
+    with pytest.raises(ClickException) as e:
+        utils.verify_filename(invalid_path[0])
