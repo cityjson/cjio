d6bfc6d4af3755d4f931d65e86724e876a40c980
jliempt <jordi_purdi@live.nl>
2021-05-18 16:56:44 +0200

    Add metadata test

---
+++ tests/test_metadata.py
@@ -0,0 +1,21 @@
+"""Test metadata
+
+"""
+import pytest
+import copy
+from cjio import cityjson
+
+
+
+class TestMetadata:
+    def test_update_metadata(self, delft):
+        cm = copy.deepcopy(delft)
+        cm.update_metadata(overwrite=True)
+        assert len(cm.j['metadata']['cityfeatureMetadata']) == len(delft.j['metadata']['cityfeatureMetadata'])
+        for type in cm.j['metadata']['cityfeatureMetadata']:
+            assert cm.j['metadata']['cityfeatureMetadata'][type]['uniqueFeatureCount'] == delft.j['metadata']['cityfeatureMetadata'][type]['uniqueFeatureCount']
+            assert cm.j['metadata']['cityfeatureMetadata'][type]['aggregateFeatureCount'] == delft.j['metadata']['cityfeatureMetadata'][type]['aggregateFeatureCount']
+            assert cm.j['metadata']['cityfeatureMetadata'][type]['presentLoDs']["1"] == delft.j['metadata']['cityfeatureMetadata'][type]['presentLoDs']["1"]
+        
+        assert cm.j['metadata']['presentLoDs'] == delft.j['metadata']['presentLoDs']
+        assert cm.j['metadata']['geographicalExtent'] == delft.j['metadata']['geographicalExtent']
