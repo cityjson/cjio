880f84142e60ee9e1d58a84621a12a31cbfb7410
Balázs Dukai <balazs.dukai@gmail.com>
2021-12-02 10:08:08 +0100

    Bump version: 0.7.1 → 0.7.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Hugo Ledoux'
 author = 'Hugo Ledoux, Balázs Dukai'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.1'
+release = '0.7.2'
 
 
 # -- General configuration ---------------------------------------------------
