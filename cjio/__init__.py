2708732341bf22e03b57da1199fbc393446726a2
GinaStavropoulou <ginastavropoulou@gmail.com>
2024-10-30 12:29:11 +0100

    deprecation warning

--- cjio/__init__.py
+++ cjio/__init__.py
@@ -1,17 +1,18 @@
 __version__ = "0.9.0"
 import importlib
 
-loader = importlib.find_loader("triangle")
+
+loader = importlib.util.find_spec("triangle")
 MODULE_TRIANGLE_AVAILABLE = loader is not None
 
-loader = importlib.find_loader("mapbox_earcut")
+loader = importlib.util.find_spec("mapbox_earcut")
 MODULE_EARCUT_AVAILABLE = loader is not None
 
-loader = importlib.find_loader("pyproj")
+loader = importlib.util.find_spec("pyproj")
 MODULE_PYPROJ_AVAILABLE = loader is not None
 
-loader = importlib.find_loader("pandas")
+loader = importlib.util.find_spec("pandas")
 MODULE_PANDAS_AVAILABLE = loader is not None
 
-loader = importlib.find_loader("cjvalpy")
+loader = importlib.util.find_spec("cjvalpy")
 MODULE_CJVAL_AVAILABLE = loader is not None
