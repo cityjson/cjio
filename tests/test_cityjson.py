e75d1eb40bb722ac9d249350d822805dce18e659
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-05-02 16:08:00 +0200

    removing unused functions

--- tests/test_cityjson.py
+++ tests/test_cityjson.py
@@ -7,49 +7,6 @@ from math import isclose
 import json
 
 class TestCityJSON:
-    # def test_get_cityobjects_type(self, cm_zur_subset):
-    #     cm = cm_zur_subset
-    #     buildings = cm.get_cityobjects(type="building")
-    #     assert len(buildings) > 0 and all(
-    #         [co.type == "Building" for i, co in buildings.items()]
-    #     )
-    #     buildings_parts = cm.get_cityobjects(type=["building", "buildingpart"])
-    #     types = [co.type for i, co in buildings_parts.items()]
-    #     assert ("Building" in types) and ("BuildingPart" in types)
-
-    # def test_get_cityobjects_ids(self, cm_zur_subset):
-    #     cm = cm_zur_subset
-    #     res_id = [
-    #         "UUID_2e5320be-a782-4517-bd0e-ab2cc2407649",
-    #         "UUID_942e02c4-45cc-4d51-bdde-625df1c81410",
-    #     ]
-    #     buildings = cm.get_cityobjects(
-    #         id=[
-    #             "UUID_2e5320be-a782-4517-bd0e-ab2cc2407649",
-    #             "UUID_942e02c4-45cc-4d51-bdde-625df1c81410",
-    #         ]
-    #     )
-    #     assert len(buildings) > 0 and all(
-    #         [co.id in res_id for i, co in buildings.items()]
-    #     )
-
-    # def test_get_cityobjects_all(self, cm_zur_subset):
-    #     cm = cm_zur_subset
-    #     all_cos = cm.get_cityobjects()
-    #     assert len(all_cos) == len(cm.cityobjects)
-
-    # def test_reference_geometry(self, cm_zur_subset):
-    #     """Test build a coordinate list and index the vertices"""
-    #     cm = cm_zur_subset
-    #     cityobjects, vertex_lookup = cm.reference_geometry()
-    #  assert len(cityobjects) == len(cm.j["CityObjects"])
-
-    def test_get_children(self):
-        """# TODO BD: Get all children of a CityObject"""
-
-    def test_get_parents(self):
-        """# TODO BD: Get all parents of a CityObject"""
-
     def test_subset_ids(self, zurich_subset):
         # Parent ID
         subset = zurich_subset.get_subset_ids(
